cd0f192b7fbc1a40518e0157ae995603d0cc824f
ehrrrr <elisaveta.s.hristova@gmail.com>
2019-07-16 19:27:06 +0300

    Updates

---
+++ precache-manifest.a4540cba7b4e68e4e27111aaec9c6d84.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a50a79d3a2b718abb4cff905e31aaa42",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "7eb6581693780d6e8f4b",
+    "url": "/robofriends/static/css/main.dc91276e.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "7eb6581693780d6e8f4b",
+    "url": "/robofriends/static/js/main.54c0aed4.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "16b3faf6e5026565d76ec321944b0fde",
+    "url": "/robofriends/static/media/SEGA.16b3faf6.svg"
+  },
+  {
+    "revision": "5600967edd3f1a027665ba2572f23016",
+    "url": "/robofriends/static/media/SEGA.5600967e.woff"
+  },
+  {
+    "revision": "9763f5e81b3e7aca539592a765a86dc3",
+    "url": "/robofriends/static/media/SEGA.9763f5e8.eot"
+  },
+  {
+    "revision": "b30fce6c8cd2c7ddeb9e0acb764894b0",
+    "url": "/robofriends/static/media/SEGA.b30fce6c.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.720bf68b4be8cf65171a5590db09bdcb.js"
+  "/robofriends/precache-manifest.a4540cba7b4e68e4e27111aaec9c6d84.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
