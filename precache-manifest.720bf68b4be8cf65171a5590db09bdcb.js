402549a63d2020b077fc0342f57702d72ff0de21
ehrrrr <elisaveta.s.hristova@gmail.com>
2019-07-16 19:12:03 +0300

    Updates

---
+++ precache-manifest.720bf68b4be8cf65171a5590db09bdcb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf966338f9db1ed79914d9a41224733",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "./static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "f27f2bedf14d3b6b2e8b",
+    "url": "./static/css/main.c92d3207.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "./static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "f27f2bedf14d3b6b2e8b",
+    "url": "./static/js/main.481b6352.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "16b3faf6e5026565d76ec321944b0fde",
+    "url": "./static/media/SEGA.16b3faf6.svg"
+  },
+  {
+    "revision": "5600967edd3f1a027665ba2572f23016",
+    "url": "./static/media/SEGA.5600967e.woff"
+  },
+  {
+    "revision": "9763f5e81b3e7aca539592a765a86dc3",
+    "url": "./static/media/SEGA.9763f5e8.eot"
+  },
+  {
+    "revision": "b30fce6c8cd2c7ddeb9e0acb764894b0",
+    "url": "./static/media/SEGA.b30fce6c.ttf"
+  }
+]);
